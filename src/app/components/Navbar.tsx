974ac9c21a32dee7e64fe8d81b506163c79af777
JeetDas5 <jeet15083011@gmail.com>
2024-12-18 17:24:23 +0530

    fixed build error

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -24,20 +24,20 @@ const Navbar = () => {
           <p className="text-white m-2 text-xl ">
             Welcome {user.name}
           </p>
-          <a
+          <Link
             href="/api/auth/logout"
             className="text-green-800 text-lg bg-white px-4 py-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-primary hover:text-primary-foreground"
           >
             Logout
-          </a>
+          </Link>
           </div>
         ) : (
-          <a
+          <Link
             href="/api/auth/login"
             className="text-green-800 text-lg bg-white px-4 py-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-primary hover:text-primary-foreground"
           >
             Login
-          </a>
+          </Link>
         )}
 
           
